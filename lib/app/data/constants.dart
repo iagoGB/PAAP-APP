b4d55bda7084fa779ea20d4327fa4dddb58b5eb7
Iago Gomes <iagogb@alu.ufc.br>
2022-01-20 12:04:06 -0300

    Criação e edição de usuário

--- lib/app/data/constants.dart
+++ lib/app/data/constants.dart
@@ -1,3 +1,4 @@
-const BASE_URL = "http://192.168.3.111:9999/api";
-// 'https://paap-ufc.herokuapp.com/api';
+const BASE_URL = 'https://paap-ufc.herokuapp.com/api';
+// "http://192.168.3.111:9999/api";
+
 const STORAGE_KEYNAME = 'PAAP';
